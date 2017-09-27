8b59ddf9f962efe81c0ac661542ce8322c64d610
Mário de Sá Vera <mario.vera@b2wdigital.com>
2017-09-27 09:57:33 -0300

    started api

---
+++ dev/api/src/main/java/com/krushweb/apiWeb/ApiWebApplication.java
@@ -0,0 +1,12 @@
+package com.krushweb.apiWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWebApplication.class, args);
+	}
+}
