9f0839ee4456a6e4d6a96f20640fd707c78f4e1d
Mário de Sá Vera <mario.vera@b2wdigital.com>
2018-01-11 00:42:18 -0200

    new models but several questions

--- dev/krushwebapi/api/models/KrushwebModel.js
+++ dev/krushwebapi/api/models/KrushwebModel.js
@@ -2,11 +2,85 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var LeadSchema = new Schema({
-  client_id: {
+
+var LeadEventTypeSchema = new Schema({
+  name: {
     type: String,
-    required: 'lead client identification is required'
+    required: 'lead event type name is required'
   },
+  integration: {
+    type: String,
+    enum : ['salesforce', 'google agenda', 'total voice', 'twillio']
+  }
+});
+var ActivitySchema = new Schema({
+  subject: {
+    type: String,
+    required: 'activity subject is required'
+  },
+  atype: {
+    type: String,
+    enum : ['notas','ligações','reuniões','comentários','agendamentos']
+    required: 'activity atype is required'
+  },
+  due_timestamp: {
+    type: String,
+    required: 'activity due timestamp is required'
+  },
+  duration: {
+    type: Number,
+    required: 'activity due timestamp is required'
+  },
+  metaclient_id: {type : Schema.ObjectId, ref : 'MetaClient'}
+});
+
+var DealSchema = new Schema({
+  status: {
+    type: String,
+    required: 'deal status is required'
+  },
+  currency: {
+    type: String,
+    required: 'deal currency is required'
+  },
+  metaclient_id: {type : Schema.ObjectId, ref : 'MetaClient'},
+  followers: {
+    type: "array",
+    items: {
+      type: "string"
+    },
+    "minItems": 1,
+    "uniqueItems": true
+  }
+});
+
+var MetaClientSchema = new Schema({
+  name: {
+    type: String,
+    required: 'user name is required',
+    unique: true
+  },
+  metadata: Schema.Types.Mixed
+});
+
+var ClientSchema = new Schema({
+  address: {
+    type: String,
+    required: 'client address is required'
+  },
+  name: {
+    type: String,
+    required: 'user name is required',
+    unique: true
+  },
+  phone: {
+    type: String,
+    unique: true
+  }
+});
+
+var LeadSchema = new Schema({
+  metaclient_id: {type : Schema.ObjectId, ref : 'MetaClient'},
   email: {
     type: String,
     required: 'lead email is required'
@@ -17,6 +91,7 @@ var LeadSchema = new Schema({
   },
   source: {
     type: String,
+    enum : ['origem','landing-page','facebook bot','site'],
     required: 'lead source is required'
   },
   campaign: {
@@ -65,18 +140,15 @@ var UserSchema = new Schema({
     required: 'user email is required',
     unique: true
   },
+  phone: {
+    type: String,
+    unique: true
+  },
   passwd: {
     type: String,
     required: 'user password is required'
   },
-  metadata: {
-    type: "array",
-    items: {
-      type: "string"
-    },
-    "minItems": 1,
-    "uniqueItems": true
-  },
+  client_id: {type : Schema.ObjectId, ref : 'Client'}
 });
 
 var SessionsSchema = new Schema({
@@ -101,3 +173,8 @@ var SessionsSchema = new Schema({
 module.exports = mongoose.model('Leads', LeadSchema);
 module.exports = mongoose.model('Users', UserSchema);
 module.exports = mongoose.model('Sessions', SessionsSchema);
+module.exports = mongoose.model('Clients', ClientSchema);
+module.exports = mongoose.model('MetaClients', MetaClientSchema);
+module.exports = mongoose.model('Deals', DealSchema);
+module.exports = mongoose.model('Activities', ActivitySchema);
+module.exports = mongoose.model('LeadEventTypes', LeadEventTypeSchema);
