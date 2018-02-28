7ff0b8c066159c70e4866f89e696421ac8f98ff3
Mario Vera <desavera@gmail.com>
2018-02-28 14:41:25 -0300

    TDD in the house

---
+++ dev/krushwebapi/test/deal-tests.js
@@ -0,0 +1,37 @@
+var chai = require('chai');
+var mongoose = require('mongoose');
+var chaiHttp = require('chai-http');
+//var server = require('../app'); // my express app
+var should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Krushweb API Tests', function() {
+ /* before(function() {
+    mongoose.createConnection('mongodb://localhost/bot-test', myOptionsObj);
+  });
+
+  beforeEach(function(done) {
+    // I do stuff like populating db
+  });
+
+  afterEach(function(done) {
+    // I do stuff like deleting populated db
+  });
+
+  after(function() {
+    mongoose.connection.close();
+  });
+*/
+  describe('Deals', function() {
+
+    it.only('should list ALL deals on /deals GET', function(done) {
+      chai.request("http://localhost:8080")
+        .get('/deals')
+        .end(function(err, res){
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+});
